bad9b9b073c540876b2db19a3adc194e94a42888
takahashi yoshiaki <s1942065fy@s.chibakoudai.jp>
2021-06-30 11:54:24 +0900

    start

---
+++ htdocs/_common.php
@@ -0,0 +1,19 @@
+<?php
+
+//商品表示のプルダウン入力
+function pulldown_disp()
+{
+	print '<select name="procode">';
+	print '<option value="1">1</option>';
+	print '<option value="2">2</option>';
+	print '<option value="3">3</option>';
+	print '<option value="4">4</option>';
+	print '<option value="5">5</option>';
+	print '<option value="6">6</option>';
+	print '<option value="7">7</option>';
+	print '<option value="8">8</option>';
+	print '<option value="9">9</option>';
+	print '<option value="10">10</option>';
+	print '</select>';
+}
+
