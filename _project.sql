cbd597142fc79e5e44226d169a07c3aed0d0700b
shimoda-lab <shimoda4@users.noreply.github.com>
2021-07-08 20:55:40 +0900

    202107081

---
+++ _project.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2021-06-20 04:08:44
+-- サーバのバージョン： 10.4.19-MariaDB
+-- PHP のバージョン: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `login_data`
+--
+
+CREATE TABLE `login_data` (
+  `code` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `login_data`
+--
+
+INSERT INTO `login_data` (`code`, `name`, `password`) VALUES
+(1, 'chiba', 'koudai');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `mst_product`
+--
+
+CREATE TABLE `mst_product` (
+  `code` int(11) NOT NULL,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `gazou` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `mst_product`
+--
+
+INSERT INTO `mst_product` (`code`, `name`, `price`, `gazou`) VALUES
+(1, 'きゃべつ', 100, ''),
+(2, 'とまと', 1000, ''),
+(3, 'みかん', 500, ''),
+(4, 'すいか', 200, '');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `login_data`
+--
+ALTER TABLE `login_data`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- テーブルのインデックス `mst_product`
+--
+ALTER TABLE `mst_product`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `login_data`
+--
+ALTER TABLE `login_data`
+  MODIFY `code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- テーブルの AUTO_INCREMENT `mst_product`
+--
+ALTER TABLE `mst_product`
+  MODIFY `code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
